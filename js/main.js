7861ba46c4841ae9e1d6366febaa174e3b01b1ca
Corey <corey.hampton@student.awesomeinc.org>
2022-03-09 21:42:27 +0000

    Update to working version

--- js/main.js
+++ js/main.js
@@ -4,10 +4,8 @@ class Model {
         this.title = "Tic Tac Toe";
         this.scoreX = 0;
         this.scoreO = 0;
-        this.turn = "X's Turn";
-        this.board = [0, 0, 0,
-            0, 0, 0,
-            0, 0, 0];
+        this.turn = "X";
+        this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         this.possibleWins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
         this.endGame = false;
     }
@@ -17,11 +15,17 @@ class Model {
     getTitle() {
         return this.title;
     }
-    setScore(x) {
-        this.score = x;
+    setScoreX(x) {
+        this.scoreX = x;
     }
-    getScore() {
-        return this.score;
+    getScoreX() {
+        return this.scoreX;
+    }
+    setScoreO(x) {
+        this.scoreO = x;
+    }
+    getScoreO() {
+        return this.scoreO;
     }
     setTurn(x) {
         this.turn = x;
@@ -47,6 +51,12 @@ class Model {
     getEndGame() {
         return this.endGame;
     }
+    resetGame(init) {
+        this.setBoard([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+        this.setTurn("X");
+        this.setEndGame(false);
+        init
+    }
 }
 
 // View
@@ -56,8 +66,9 @@ class View {
         this.body = document.body;
 
     }
-    render(data, fn) {
-        console.log(data)
+    render(data, fn, turnSwitch, resetGame, init, addGame) {
+        console.log("RENDERING")
+        // document.body.innerHTML = ""
 
         this.titleContainer = document.createElement("div");
         this.titleContainer.id = "titleContainer"
@@ -66,22 +77,25 @@ class View {
         this.board.classList = 'board'
         this.scoreContainer = document.createElement("div");
         this.scoreContainer.id = "scoreContainer"
+        this.resetContainer = document.createElement("div");
+        this.resetContainer.id = "resetContainer"
 
         this.body.appendChild(this.titleContainer)
         this.body.appendChild(this.scoreContainer)
         this.body.appendChild(this.board)
+        this.body.appendChild(this.resetContainer)
 
         this.titleDisplay = document.createElement("h1");
-        this.titleDisplay.innerText = data.title;
+        this.titleDisplay.innerText = data.getTitle();
         this.titleContainer.appendChild(this.titleDisplay)
         this.scoreDisplay = document.createElement("div");
-
+        
         this.scoreDisplay = document.createElement("h1");
-        this.scoreDisplay.innerText = `Score X: ${data.scoreX}`
+        this.scoreDisplay.innerText = `Score X: ${data.getScoreX()}`
         this.scoreContainer.appendChild(this.scoreDisplay)
         this.scoreDisplay = document.createElement("div");
         this.scoreDisplay = document.createElement("h1");
-        this.scoreDisplay.innerText = `Score O: ${data.scoreO}`
+        this.scoreDisplay.innerText = `Score O: ${data.getScoreO()}`
         this.scoreContainer.appendChild(this.scoreDisplay)
         this.scoreDisplay = document.createElement("div");
         // Tiles
@@ -89,11 +103,16 @@ class View {
             let tile = document.createElement('button')
             tile.classList = `tile`
             tile.id = `tile${i}`
-            tile.addEventListener('click', (e) => { e.target.disabled = true; e.target.innerText = "X"; fn(e.target.id) })
-            board.append(tile)
+            tile.addEventListener('click', (e) => { e.target.disabled = true; e.target.innerText = data.getTurn(); fn(e.target.id); turnSwitch()})
+            this.board.append(tile)
         }
+        this.resetDisplay = document.createElement("button");
+        this.resetDisplay.innerText = "RESET";
+        this.resetDisplay.addEventListener('click', ()=>{addGame()});
+        this.resetContainer.appendChild(this.resetDisplay)
+        this.scoreDisplay = document.createElement("div");
     }
-
+    
 }
 
 // Controller
@@ -107,14 +126,13 @@ class Controller {
 
     init = () => {
         console.log("page loaded");
-        this.v.render(this.m, this.updateBoard);
+        this.v.render(this.m, this.updateBoard, this.turnSwitch, this.resetGame, this.init, this.addGame);
     }
     updateBoard = (x) => {
         let pos = x.charAt(x.length - 1)
-        console.log(pos);
-        this.m.board[pos] = 'X';
-        console.log(this.m.board);
-        this.checkForWin(this.m.possibleWins, this.m.board);
+        this.m.getBoard()[pos] = this.m.getTurn();
+        // console.log(this.m.board);
+        this.checkForWin(this.m.getPossibleWins(), this.m.getBoard());
     }
     checkForWin = (possible, current) => {
         for (let i = 0; i < possible.length; i++) { //For Every array in possibleWins
@@ -128,6 +146,25 @@ class Controller {
                 console.log("GAME WON!!!")
             }
         }
+    }
+    turnSwitch = () => {
+        // console.log("what is get turn doing?", this.m.getTurn())
+        if(this.m.getTurn() === "X"){
+            this.m.setTurn("O")
+            // console.log(this.m.turn)
+        } else {
+            this.m.setTurn("X")
+            // console.log(this.m.turn)
+        }
+    }
+    resetGame = () => {
+        this.m.resetGame(globalInit);
+        this.v.render(this.m, this.updateBoard, this.turnSwitch, this.resetGame, this.init);
+        
+    }
+
+    addGame = () => {
+        globalInit();
     }
 }
 /* 
